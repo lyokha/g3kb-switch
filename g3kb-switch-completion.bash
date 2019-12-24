a3845c42702293af2852fb08ab71bc73cf5094c6
lyokha <alexey.radkov@gmail.com>
2019-12-24 18:28:22 +0300

    various fixes

--- g3kb-switch-completion.bash
+++ g3kb-switch-completion.bash
@@ -7,15 +7,20 @@ _g3kb_switch_completion()
     fi
 
     local comp_opt="-h -p -l -n -s"
+    local oldifs=$IFS
 
     if [ "${#COMP_WORDS[@]}" -eq 3 ] ; then
         if [ "${COMP_WORDS[1]}" = "-s" ] ; then
-            COMPREPLY=($(compgen -W "$(g3kb-switch -l)" -- "${COMP_WORDS[2]}"))
+            IFS=$'\n'
+            COMPREPLY=( $(compgen -W "$(g3kb-switch -l |
+                          sed -e "s/\([[:blank:]]\)/\x5C\x5C\1/g")" \
+                          -- "${COMP_WORDS[2]}") )
+            IFS=$oldifs
         fi
     else
-        COMPREPLY=($(compgen -W "$comp_opt" -- "${COMP_WORDS[1]}"))
+        COMPREPLY=( $(compgen -W "$comp_opt" -- "${COMP_WORDS[1]}") )
     fi
 }
 
-complete -F _g3kb_switch_completion g3kb-switch -o nosort
+complete -F _g3kb_switch_completion -o nosort g3kb-switch
 
