7ac717de4f8e1df32a9e911afb2d22089185499c
Лёха <alexey.radkov@gmail.com>
2019-12-25 11:46:31 +0300

    minor improvements

--- g3kb-switch-completion.bash
+++ g3kb-switch-completion.bash
@@ -6,18 +6,18 @@ _g3kb_switch_completion()
         return
     fi
 
-    local comp_opt="-h -p -l -n -s"
-
     if [ "${#COMP_WORDS[@]}" -eq 3 ] ; then
         if [ "${COMP_WORDS[1]}" = "-s" ] ; then
             local IFS=$'\n'
-            local cur=${COMP_WORDS[2]}
-            local list
-            list=$(g3kb-switch -l)
+            local -r cur=${COMP_WORDS[2]}
+            local -r list=$(g3kb-switch -l)
+            # it is very unlikely that names of layout groups may contain
+            # spaces, nevertheless we prepare to process such names correctly
             COMPREPLY=( $(compgen -W "${list// /\\\\ }" -- "${cur// /\\\\ }") )
         fi
     else
         local IFS=$' \t\n'
+        local -r comp_opt="-h -p -l -n -s"
         COMPREPLY=( $(compgen -W "$comp_opt" -- "${COMP_WORDS[1]}") )
     fi
 }
