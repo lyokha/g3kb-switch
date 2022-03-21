9ffae91028d23c8293bc9d568c90b2fd2506dc7b
Freed-Wu <wuzy01@qq.com>
2022-03-21 20:49:43 +0800

    added "default" for zsh completion file

--- g3kb-switch-completion.zsh
+++ g3kb-switch-completion.zsh
@@ -6,4 +6,4 @@ _arguments \
   "(- *)"-s"[Switch to layout group ARG]:(ARG):($(g3kb-switch -l))" \
   "(- *)"-n"[Switch to the next layout group]" \
   "(- *)"-l"[Show available layout groups]" \
-  "(- *)"-p"[Show the current layout group]" \
+  "(- *)"-p"[Show the current layout group (default)]" \
