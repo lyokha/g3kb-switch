77f1369b9b40357f3bc600938e0c80b0a59fa261
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-04-24 01:15:04 +0800

    Use enum to replace many gboolean values of main.c

--- main.c
+++ main.c
@@ -41,10 +41,13 @@ int main( int argc, char **argv )
     GTree *layouts;
     gchar *new_layout = NULL;
     gconstpointer layout;
-    gboolean print_layouts = FALSE;
-    gboolean display_current_layout = FALSE;
-    gboolean activate_new_layout = FALSE;
-    gboolean activate_next_layout = FALSE;
+    enum command {
+        display_current_layout,
+        print_layouts,
+        activate_new_layout,
+        activate_next_layout,
+    };
+    enum command cmd = display_current_layout;
     GError *err = NULL;
 
     if ( argc > 1 ) {
@@ -57,24 +60,22 @@ int main( int argc, char **argv )
             version();
             return 0;
         } else if ( g_strcmp0( argv[ 1 ], "-l" ) == 0 ) {
-            print_layouts = TRUE;
+            cmd = print_layouts;
         } else if ( g_strcmp0( argv[ 1 ], "-p" ) == 0 ) {
-            display_current_layout = TRUE;
+            cmd = display_current_layout;
         } else if ( g_strcmp0( argv[ 1 ], "-s" ) == 0 ) {
             if ( argc < 3 ) {
                 usage();
                 return 1;
             }
-            activate_new_layout = TRUE;
+            cmd = activate_new_layout;
             new_layout = argv[ 2 ];
         } else if ( g_strcmp0( argv[ 1 ], "-n" ) == 0 ) {
-            activate_next_layout = TRUE;
+            cmd = activate_next_layout;
         } else {
             usage();
             return 1;
         }
-    } else {
-        display_current_layout = TRUE;
     }
 
     layouts = g3kb_build_layouts_map( &err );
@@ -85,37 +86,40 @@ int main( int argc, char **argv )
         return 1;
     }
 
-    if ( print_layouts ) {
+    int status = 0;
+    switch ( cmd ) {
+    case print_layouts:
         g_tree_foreach( layouts, g3kb_print_layouts, NULL );
-    } else if ( display_current_layout ) {
+        break;
+    case display_current_layout:
         layout = g3kb_safe_get_layout( layouts, &err );
         if ( layout == NULL ) {
             g_printerr( "Failed to find current layout: %s\n",
                         err == NULL ? "<no details>" : err->message );
             g_clear_error( &err );
-            g_tree_unref( layouts );
-            return 1;
+            status = 1;
+            break;
         }
         g_print( "%s\n", (char *)layout );
-    } else if ( activate_new_layout ) {
+        break;
+    case activate_new_layout:
         if ( !g3kb_safe_set_layout( layouts, new_layout, &err ) ) {
             g_printerr( "Failed to activate layout %s: %s\n", new_layout,
                         err == NULL ? "<no details>" : err->message );
             g_clear_error( &err );
-            g_tree_unref( layouts );
-            return 1;
+            status = 1;
         }
-    } else if ( activate_next_layout ) {
+        break;
+    case activate_next_layout:
         if ( !g3kb_set_next_layout( layouts, &err ) ) {
             g_printerr( "Failed to activate next layout: %s\n",
                         err == NULL ? "<no details>" : err->message );
             g_clear_error( &err );
-            g_tree_unref( layouts );
-            return 1;
+            status = 1;
         }
     }
 
     g_tree_unref( layouts );
 
-    return 0;
+    return status;
 }
